78655ed887b602dbfd42ce0fc8f0b856a107a480
TriciaF <TriciaF@thinkful.com>
2018-01-03 15:29:14 -0500

    completed the endpoint drils using knex

--- server.js
+++ server.js
@@ -9,8 +9,16 @@ const app = express();
 
 app.get('/restaurants', (req, res) => {
   knex.select('id', 'name', 'cuisine', 'borough')
+    .select(knex.raw('CONCAT(address_building_number, \' \', address_street, \' \', address_zipcode ) as address'))
     .from('restaurants')
     .limit(10)
+    .then(results => res.json(results));
+});
+
+app.get('/restaurants/:id', (req, res) => {
+  knex.select('name', 'id', 'cuisine', 'borough')
+    .from('restaurants')
+    .where('id', req.params.id)
     .then(results => res.json(results));
 });
 
