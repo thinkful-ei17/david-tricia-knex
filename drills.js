c4ee6b2f7911db9a1548fb8df6c1fd73ae33f27b
David Park <dysp92@gmail.com>
2018-01-03 13:06:32 -0800

    finished hydrating

--- drills.js
+++ drills.js
@@ -77,16 +77,95 @@ process.stdout.write('\x1Bc');
 //   .del()
 //   .then(results => console.log(results));
 
-knex.schema.createTableIfNotExists('inspectors', function(table) {
-  table.increments();
-  table.string('first_name').notNullable();
-  table.string('last_name').notNullable();
-  table.string('borough');
-}).then(() => {
-  return knex('inspectors')
-    .insert({ first_name: 'Tricia', last_name: 'Forrester', borough: 'Queens' });
-}).then(() => { // Destroy the connection pool
-  knex.destroy().then(() => {
-    console.log('database connection closed');
+// knex.schema.createTableIfNotExists('inspectors', function (table) {
+//   table.increments();
+//   table.string('first_name').notNullable();
+//   table.string('last_name').notNullable();
+//   table.string('borough');
+// }).then(() => {
+//   return knex('inspectors')
+//     .insert({ first_name: 'Tricia', last_name: 'Forrester', borough: 'Queens' });
+// }).then(() => { // Destroy the connection pool
+//   knex.destroy().then(() => {
+//     console.log('database connection closed');
+//   });
+// });
+
+// const people = [
+//   {
+//     id: 2,
+//     name: 'John Doe',
+//     age: 34,
+//     petName: 'Rex',
+//     petType: 'dog',
+//   },
+//   {
+//     id: 2,
+//     name: 'John Doe',
+//     age: 34,
+//     petName: 'Rex',
+//     petType: 'dog',
+//   },
+//   {
+//     id: 3,
+//     name: 'Mary Jane',
+//     age: 19,
+//     petName: 'Mittens',
+//     petType: 'kitten',
+//   },
+//   {
+//     id: 3,
+//     name: 'Mary Jane',
+//     age: 19,
+//     petName: 'Fluffy',
+//     petType: 'cat'
+//   }
+// ];
+
+// const hydrated = {};
+// people.forEach(row => {
+//   if (!(row.id in hydrated)) {
+//     hydrated[row.id] = {
+//       id: row.id,
+//       name: row.name,
+//       age: row.age,
+//       pets: []
+//     };
+//   }
+//   hydrated[row.id].pets.push({
+//     name: row.petName,
+//     type: row.petType,
+//   });
+// });
+// let str = JSON.stringify(hydrated);
+// console.log(str);
+// console.log(hydrated);
+
+const hydrated = {};
+
+knex.select('restaurants.id', 'name', 'cuisine', 'borough', 'grades.id as gradeId', 'grade', 'score')
+  .from('restaurants')
+  .innerJoin('grades', 'restaurants.id', 'grades.restaurant_id')
+  .orderBy('date', 'desc')
+  .limit(10)
+  .then(results => {
+    results.forEach(row => {
+      if (!(row.id in hydrated)) {
+        hydrated[row.id] = {
+          name: row.name,
+          cuisine: row.cuisine,
+          borough: row.borough,
+          grades: []
+        };
+      }
+      hydrated[row.id].grades.push({
+        gradeId: row.gradeId,
+        grade: row.grade,
+        score: row.score
+      });
+    });
+    let str = JSON.stringify(hydrated);
+    console.log(str);
+    console.log(JSON.parse(JSON.stringify(hydrated)));
   });
-});
+
--- server.js
+++ server.js
@@ -7,13 +7,13 @@ const knex = require('knex')(DATABASE);
 
 const app = express();
 
-app.get('/restaurants', (req, res) => {
-  knex.select('id', 'name', 'cuisine', 'borough')
-    .select(knex.raw('CONCAT(address_building_number, \' \', address_street, \' \', address_zipcode ) as address'))
-    .from('restaurants')
-    .limit(10)
-    .then(results => res.json(results));
-});
+// app.get('/restaurants', (req, res) => {
+//   knex.select('id', 'name', 'cuisine', 'borough')
+//     .select(knex.raw('CONCAT(address_building_number, \' \', address_street, \' \', address_zipcode ) as address'))
+//     .from('restaurants')
+//     .limit(10)
+//     .then(results => res.json(results));
+// });
 
 app.get('/restaurants/:id', (req, res) => {
   knex.select('name', 'id', 'cuisine', 'borough')
@@ -22,6 +22,34 @@ app.get('/restaurants/:id', (req, res) => {
     .then(results => res.json(results));
 });
 
+const hydrated = {};
+app.get('/restaurants', (req, res) => {
+  knex.select('restaurants.id', 'name', 'cuisine', 'borough', 'grades.id as gradeId', 'grade', 'score')
+    .from('restaurants')
+    .innerJoin('grades', 'restaurants.id', 'grades.restaurant_id')
+    .orderBy('date', 'desc')
+    .limit(10)
+    .then(results => {
+      results.forEach(row => {
+        if (!(row.id in hydrated)) {
+          hydrated[row.id] = {
+            name: row.name,
+            cuisine: row.cuisine,
+            borough: row.borough,
+            grades: []
+          };
+        }
+        hydrated[row.id].grades.push({
+          gradeId: row.gradeId,
+          grade: row.grade,
+          score: row.score
+        });
+      });
+      let str = JSON.stringify(hydrated);
+      console.log(str);
+      res.json(hydrated);
+    });
+});
 // ADD ANSWERS HERE
 
 app.listen(PORT);
