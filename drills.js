78169de27fb85c7c7b4cc70ba9965f01dc3a6c97
TriciaF <TriciaF@thinkful.com>
2018-01-03 15:02:12 -0500

    added promise

--- drills.js
+++ drills.js
@@ -56,7 +56,7 @@ process.stdout.write('\x1Bc');
 // knex('restaurants')
 //   .insert({name: 'Byte Cafe', borough: 'Brooklyn', cuisine: 'coffee', address_building_number: '123', address_street: 'Atlantic Avenue', address_zipcode: '11231'})
 //   .then(results => console.log(results));
- 
+
 // knex.select('id', 'name')
 //   .from('restaurants')
 //   .insert({name: 'SoCal Poke', cuisine: 'Poke'})
@@ -66,14 +66,28 @@ process.stdout.write('\x1Bc');
 //   .update('name', 'DJ Reynolds Pub and Restaurant')
 //   .where('nyc_restaurant_id', '30191841')
 //   .then(results => console.log(results));
- 
-knex('grades')
-  .where('id', 11)
-  .del()
-  .then(results => console.log(results));
-  
 
-// Destroy the connection pool
-knex.destroy().then(() => {
-  console.log('database connection closed');
+// knex('grades')
+//   .where('id', 11)
+//   .del()
+//   .then(results => console.log(results));
+
+// knex('restaurants')
+//   .where('id', 22)
+//   .del()
+//   .then(results => console.log(results));
+
+knex.schema.createTableIfNotExists('inspectors', function(table) {
+  table.increments();
+  table.string('first_name').notNullable();
+  table.string('last_name').notNullable();
+  table.string('borough');
+}).then(() => {
+  return knex('inspectors')
+    .insert({ first_name: 'Tricia', last_name: 'Forrester', borough: 'Queens' })
+    .then(results => console.log(results));
+}).then(() => { // Destroy the connection pool
+  knex.destroy().then(() => {
+    console.log('database connection closed');
+  });
 });
