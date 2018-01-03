6f90fe922382aa13b4fa014c8fb82123ddfedeca
TriciaF <TriciaF@thinkful.com>
2018-01-03 17:07:53 -0500

    completed treeize drill

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 'use strict';
 
 const express = require('express');
+const Treeize = require('treeize');
+
 
 const { DATABASE, PORT } = require('./config');
 const knex = require('knex')(DATABASE);
 
 const app = express();
+const restaurant_data = new Treeize();
 
 // app.get('/restaurants', (req, res) => {
 //   knex.select('id', 'name', 'cuisine', 'borough')
@@ -22,33 +25,50 @@ app.get('/restaurants/:id', (req, res) => {
     .then(results => res.json(results));
 });
 
-const hydrated = {};
+// const hydrated = {};
+// app.get('/restaurants', (req, res) => {
+//   knex.select('restaurants.id', 'name', 'cuisine', 'borough', 'grades.id as gradeId', 'grade', 'score')
+//     .from('restaurants')
+//     .innerJoin('grades', 'restaurants.id', 'grades.restaurant_id')
+//     .orderBy('date', 'desc')
+//     .limit(10)
+//     .then(results => {
+//       results.forEach(row => {
+//         if (!(row.id in hydrated)) {
+//           hydrated[row.id] = {
+//             name: row.name,
+//             cuisine: row.cuisine,
+//             borough: row.borough,
+//             grades: []
+//           };
+//         }
+//         hydrated[row.id].grades.push({
+//           gradeId: row.gradeId,
+//           grade: row.grade,
+//           score: row.score
+//         });
+//       });
+//       let str = JSON.stringify(hydrated);
+//       console.log(str);
+//       res.json(hydrated);
+//     });
+// });
+
+
+//using Treeize
+
+const hydrated = [];
 app.get('/restaurants', (req, res) => {
-  knex.select('restaurants.id', 'name', 'cuisine', 'borough', 'grades.id as gradeId', 'grade', 'score')
+  knex.select('restaurants.id', 'name', 'cuisine', 'borough', 'grades.id as grades:gradeId', 'grade as grades:grade', 'score as grades:score')
     .from('restaurants')
     .innerJoin('grades', 'restaurants.id', 'grades.restaurant_id')
     .orderBy('date', 'desc')
     .limit(10)
     .then(results => {
-      results.forEach(row => {
-        if (!(row.id in hydrated)) {
-          hydrated[row.id] = {
-            name: row.name,
-            cuisine: row.cuisine,
-            borough: row.borough,
-            grades: []
-          };
-        }
-        hydrated[row.id].grades.push({
-          gradeId: row.gradeId,
-          grade: row.grade,
-          score: row.score
-        });
-      });
-      let str = JSON.stringify(hydrated);
-      console.log(str);
-      res.json(hydrated);
+      restaurant_data.grow(results);
+      res.json(restaurant_data.getData());
     });
+
 });
 // ADD ANSWERS HERE
 
